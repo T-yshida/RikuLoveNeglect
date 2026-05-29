ef3d7accb960c62a7d24c89be9c3ad6102a9c896
ハチドリくん <yama.sakibill0630@gmail.com>
2026-05-29 16:24:18 +0900

    エンディングのスクリプタブルをお願いします

---
+++ Assets/Scripts/Common/SpecialEndFlag.cs
@@ -0,0 +1,26 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
+
+[CreateAssetMenu]
+public class SpecialEndFlag : ScriptableObject
+{
+    [SerializeField] public List<SFlags> SFlags = new List<SFlags>();
+    
+}
+
+[System.Serializable]
+public class SFlags
+{
+    [SerializeField] public GameManager.place place;
+    [SerializeField] public List<Flags> flags;
+}
+
+[System.Serializable]
+public class Flags
+{
+    [SerializeField] public string discription;
+    [SerializeField] public bool[] flag;
+}
