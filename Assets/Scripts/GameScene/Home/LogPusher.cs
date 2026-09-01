1bd18d5dc9a1ff5f8d07bf45713fa465fac11718
ハチドリくん <yama.sakibill0630@gmail.com>
2026-09-01 16:53:04 +0900

    feat:放置中の彼女の行動ログを追加、アウトラインのみ

---
+++ Assets/Scripts/GameScene/Home/LogPusher.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogPusher : MonoBehaviour
+{
+    
+}
+
+class LogString 
+{
+    public string[] Logs = { 
+        "",
+        ""
+    };
+}
